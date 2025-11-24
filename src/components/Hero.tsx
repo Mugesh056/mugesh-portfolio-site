37049a3629796718b41a3bcfd0cf40e71d07934d
MUGESH_G <87661079+Mugesh056@users.noreply.github.com>
2025-11-24 22:11:09 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -65,17 +65,7 @@ const Hero = () => {
                 </a>
               </Button>
 
-              <Button
-                asChild
-                size="lg"
-                variant="outline"
-                className="glass-hover border-primary/50"
-              >
-                <a href="https://www.naukri.com/" target="_blank" rel="noopener noreferrer">
-                  <FileText className="w-5 h-5 mr-2" />
-                  Naukri
-                </a>
-              </Button>
+           
             </div>
 
             {/* Contact Info */}
